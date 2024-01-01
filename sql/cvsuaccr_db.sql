68c4ac695a5cb3c750a076fa3925e467d668d09c
Laica Dennise <144158694+laica-dennise@users.noreply.github.com>
2024-01-02 00:55:35 +0800

    Add files via upload

--- sql/cvsuaccr_db.sql
+++ sql/cvsuaccr_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2023 at 10:00 PM
+-- Generation Time: Jan 01, 2024 at 05:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `access_request`
+--
+
+CREATE TABLE `access_request` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `birthdate` date NOT NULL,
+  `user_level` varchar(5) DEFAULT NULL,
+  `college` varchar(80) DEFAULT NULL,
+  `request_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `files`
 --
 
@@ -36,7 +54,9 @@ CREATE TABLE `files` (
   `file_viewLink` varchar(200) NOT NULL,
   `file_downloadLink` varchar(200) NOT NULL,
   `file_owner` varchar(200) NOT NULL,
+  `owner_email` varchar(255) NOT NULL,
   `file_directory` varchar(100) NOT NULL,
+  `file_course` varchar(255) NOT NULL,
   `file_area` varchar(50) NOT NULL,
   `file_tags` varchar(255) NOT NULL,
   `upload_date` timestamp NOT NULL DEFAULT current_timestamp()
@@ -46,10 +66,9 @@ CREATE TABLE `files` (
 -- Dumping data for table `files`
 --
 
-INSERT INTO `files` (`id`, `file_name`, `file_size`, `file_type`, `file_id`, `file_viewLink`, `file_downloadLink`, `file_owner`, `file_directory`, `file_area`, `file_tags`, `upload_date`) VALUES
-(74, 'REGISTRATION-FORM.pdf', 1185161, 'application/pdf', '1PORd2GKz6a4_XOGLj9wPxVNaMEI99YWb', 'https://drive.google.com/file/d/1PORd2GKz6a4_XOGLj9wPxVNaMEI99YWb/view?usp=drivesdk', 'https://drive.google.com/uc?id=1PORd2GKz6a4_XOGLj9wPxVNaMEI99YWb&export=download', 'Laica Dennise Miranda', 'CAFENR', 'Area 1', 'Phase 1, Phase 2, Area 3, Area 4, BSCS', '2023-12-14 16:02:10'),
-(75, '340088218_526647569462377_78141819513422517_n.jpg', 145580, 'image/jpeg', '198kQ1PvRoBJUh53o1Vom7Z8AvhIxIQ1m', 'https://drive.google.com/file/d/198kQ1PvRoBJUh53o1Vom7Z8AvhIxIQ1m/view?usp=drivesdk', 'https://drive.google.com/uc?id=198kQ1PvRoBJUh53o1Vom7Z8AvhIxIQ1m&export=download', 'Laica Dennise Miranda', 'CCJ', 'Area 1', '', '2017-12-20 14:00:07'),
-(76, 'COLLEGE-COURSES.docx', 16177, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', '1C_z4bNdRwlXA2uPaaKVS3Mj6qsLvRQ4k', 'https://docs.google.com/document/d/1C_z4bNdRwlXA2uPaaKVS3Mj6qsLvRQ4k/edit?usp=drivesdk&ouid=101333192880725710175&rtpof=true&sd=true', 'https://drive.google.com/uc?id=1C_z4bNdRwlXA2uPaaKVS3Mj6qsLvRQ4k&export=download', 'Laica Dennise Miranda', 'CCJ', 'Area 3', '', '2023-12-20 19:25:10');
+INSERT INTO `files` (`id`, `file_name`, `file_size`, `file_type`, `file_id`, `file_viewLink`, `file_downloadLink`, `file_owner`, `owner_email`, `file_directory`, `file_course`, `file_area`, `file_tags`, `upload_date`) VALUES
+(81, 'IMG_20200829_132309.jpg', 985209, 'image/jpeg', '1ZuEsy5ew_0Um37ujYwIOPpuJsKU5HPoq', 'https://drive.google.com/file/d/1ZuEsy5ew_0Um37ujYwIOPpuJsKU5HPoq/view?usp=drivesdk', 'https://drive.google.com/uc?id=1ZuEsy5ew_0Um37ujYwIOPpuJsKU5HPoq&export=download', 'Laica Dennise Miranda', 'laicadennise.miranda@gmail.com', 'CAFENR', 'Bachelor of Science in Agriculture', 'Area 1', 'BSA', '2024-01-01 15:31:58'),
+(82, 'IMG_20200829_132309.jpg', 985209, 'image/jpeg', '1ZuEsy5ew_0Um37ujYwIOPpuJsKU5HPoq', 'https://drive.google.com/file/d/1ZuEsy5ew_0Um37ujYwIOPpuJsKU5HPoq/view?usp=drivesdk', 'https://drive.google.com/uc?id=1ZuEsy5ew_0Um37ujYwIOPpuJsKU5HPoq&export=download', 'LAICA DENNISE MIRANDA', 'laicadennise.miranda@cvsu.edu.ph', 'CAFENR', 'Bachelor of Science in Agriculture', 'Area 1', 'bsa', '2017-12-31 16:29:39');
 
 -- --------------------------------------------------------
 
@@ -124,7 +143,34 @@ INSERT INTO `logs` (`id`, `email`, `user_level`, `college`, `time`, `activity`) 
 (53, 'williamdave.agravante@cvsu.edu.ph', 3, 'CAFENR', '2023-12-20 17:30:25', 'williamdave.agravante@cvsu.edu.ph has uploaded a file: Fishbone diagram worksheet - Color.png at CAFENR'),
 (54, 'williamdave.agravante@cvsu.edu.ph', 3, 'CED', '2023-12-20 17:39:20', 'File \'Agravante-checklist.pdf\' owned by \'William Dave Agravante\' in \'CED\' deleted by \'williamdave.agravante@cvsu.edu.ph\''),
 (55, 'williamdave.agravante@cvsu.edu.ph', 3, 'CAFENR', '2023-12-20 17:44:45', 'williamdave.agravante@cvsu.edu.ph has logged into the system'),
-(56, 'williamdave.agravante@cvsu.edu.ph', 3, 'CAFENR', '2023-12-20 17:50:50', 'williamdave.agravante@cvsu.edu.ph has uploaded a file: Agravante-checklist.pdf at CAFENR');
+(56, 'williamdave.agravante@cvsu.edu.ph', 3, 'CAFENR', '2023-12-20 17:50:50', 'williamdave.agravante@cvsu.edu.ph has uploaded a file: Agravante-checklist.pdf at CAFENR'),
+(57, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:13:18', 'laicadennise.miranda@gmail.com has logged into the system'),
+(58, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:18:53', 'laicadennise.miranda@gmail.com has added the user: testuser@gmail.com to the database.'),
+(59, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:23:18', 'laicadennise.miranda@gmail.com has added the user: test@gmail.com to the database'),
+(60, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:24:59', 'laicadennise.miranda@gmail.com has added the user: test@gmail.com to the database'),
+(61, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:26:01', 'laicadennise.miranda@gmail.com has uploaded a file: Pre-registration Form.docx.pdf at CAFENR'),
+(62, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:26:47', 'File \'COLLEGE-COURSES.docx\' owned by \'Laica Dennise Miranda\' in \'CEIT\' was deleted by \'laicadennise.miranda@gmail.com\' .'),
+(63, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:31:58', 'laicadennise.miranda@gmail.com has uploaded a file: IMG_20200829_132309.jpg at CAFENR'),
+(64, 'laicadennise.miranda@gmail.com', 0, 'CAFENR', '2024-01-01 15:32:34', 'File \'Pre-registration Form.docx.pdf\' owned by \'Laica Dennise Miranda\' in \'CAFENR\' was deleted by \'laicadennise.miranda@gmail.com\''),
+(65, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:33:09', 'laicadennise.miranda@gmail.com has added the user: testt@gmail.com to the database'),
+(66, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:33:22', 'laicadennise.miranda@gmail.com has deleted the user with ID: 18 from the database'),
+(67, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:37:03', 'laicadennise.miranda@gmail.com has added the user: testing@gmail.com to the database'),
+(68, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:40:51', 'laicadennise.miranda@gmail.com has deleted a user from the database'),
+(69, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:42:48', 'laicadennise.miranda@gmail.com has added the user: abc@gmail.com to the database'),
+(70, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:44:05', 'laicadennise.miranda@gmail.com has deleted a user from the database'),
+(71, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:58:55', 'laicadennise.miranda@gmail.com has logged out from the system'),
+(72, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:59:03', 'laicadennise.miranda@gmail.com has logged into the system'),
+(73, 'laicadennise.miranda@gmail.com', 0, 'CEIT', '2024-01-01 15:59:06', 'laicadennise.miranda@gmail.com has logged out from the system'),
+(74, 'laicadennise.miranda@cvsu.edu.ph', 2, 'CEIT', '2024-01-01 16:00:20', 'laicadennise.miranda@cvsu.edu.ph has logged into the system'),
+(75, 'laicadennise.miranda@cvsu.edu.ph', 2, 'CEIT', '2024-01-01 16:08:47', 'laicadennise.miranda@cvsu.edu.ph has logged into the system'),
+(76, 'laicadennise.miranda@cvsu.edu.ph', 2, 'CEIT', '2024-01-01 16:18:45', 'laicadennise.miranda@cvsu.edu.ph has logged out from the system'),
+(77, 'laicadennise.miranda@cvsu.edu.ph', 3, 'CAFENR', '2024-01-01 16:19:16', 'laicadennise.miranda@cvsu.edu.ph has logged into the system'),
+(78, 'laicadennise.miranda@cvsu.edu.ph', 3, 'CAFENR', '2024-01-01 16:29:39', 'laicadennise.miranda@cvsu.edu.ph has uploaded a file: IMG_20200829_132309.jpg at CAFENR'),
+(79, 'laicadennise.miranda@cvsu.edu.ph', 3, 'CAFENR', '2024-01-01 16:37:44', 'laicadennise.miranda@cvsu.edu.ph has logged out from the system'),
+(80, 'laicadennise.miranda@cvsu.edu.ph', 3, 'CAFENR', '2024-01-01 16:38:12', 'laicadennise.miranda@cvsu.edu.ph has logged into the system'),
+(81, 'laicadennise.miranda@cvsu.edu.ph', 3, 'CAFENR', '2024-01-01 16:40:50', 'laicadennise.miranda@cvsu.edu.ph has logged out from the system'),
+(82, 'laicadennise.miranda@cvsu.edu.ph', 1, 'CAFENR', '2024-01-01 16:41:08', 'laicadennise.miranda@cvsu.edu.ph has logged into the system'),
+(83, 'laicadennise.miranda@cvsu.edu.ph', 1, 'CAFENR', '2024-01-01 16:41:28', 'laicadennise.miranda@cvsu.edu.ph has logged out from the system');
 
 -- --------------------------------------------------------
 
@@ -143,20 +189,27 @@ CREATE TABLE `users` (
   `profile_pic` varchar(200) NOT NULL,
   `user_level` int(3) NOT NULL,
   `local` varchar(20) DEFAULT NULL,
-  `college` varchar(50) NOT NULL
+  `college` varchar(50) NOT NULL,
+  `active_status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `oauth_uid`, `first_name`, `last_name`, `gender`, `birthdate`, `email`, `profile_pic`, `user_level`, `local`, `college`) VALUES
-(2, '102958130049128401532', 'LAICA DENNISE', 'MIRANDA', '', '0000-00-00', 'laicadennise.miranda@cvsu.edu.ph', '', 3, 'https://lh3.googleus', ''),
-(3, '101333192880725710175', 'Laica Dennise', 'Miranda', 'Female', '2001-08-26', 'laicadennise.miranda@gmail.com', '', 0, 'https://lh3.googleus', 'CEIT');
+INSERT INTO `users` (`id`, `oauth_uid`, `first_name`, `last_name`, `gender`, `birthdate`, `email`, `profile_pic`, `user_level`, `local`, `college`, `active_status`) VALUES
+(2, '102958130049128401532', 'LAICA DENNISE', 'MIRANDA', 'Female', '2024-01-01', 'laicadennise.miranda@cvsu.edu.ph', '', 1, 'https://lh3.googleus', 'CAFENR', 'offline'),
+(3, '101333192880725710175', 'Laica Dennise', 'Miranda', 'Female', '2001-08-26', 'laicadennise.miranda@gmail.com', '', 0, 'https://lh3.googleus', 'CEIT', 'offline');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `access_request`
+--
+ALTER TABLE `access_request`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `files`
@@ -185,22 +238,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `access_request`
+--
+ALTER TABLE `access_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
